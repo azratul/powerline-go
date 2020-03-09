432b819167be5321d8b536d722748f71134f3a84
Claudio Lepin <claudio.lepin@uchile.cl>
2020-03-09 18:16:36 -0300

    TODO: Add cache, because it's so slow

--- segment-docker.go
+++ segment-docker.go
@@ -1,24 +1,60 @@
 package main
 
 import (
-	"net/url"
-	"os"
+	"strconv"
+	"log"
+	"fmt"
+	"strings"
+	"os/exec"
 
 	pwl "github.com/justjanne/powerline-go/powerline"
 )
 
-func segmentDocker(p *powerline) {
-	var docker string
-	dockerMachineName, _ := os.LookupEnv("DOCKER_MACHINE_NAME")
-	dockerHost, _ := os.LookupEnv("DOCKER_HOST")
 
-	if dockerMachineName != "" {
-		docker = dockerMachineName
-	} else if dockerHost != " " {
-		u, err := url.Parse(dockerHost)
-		if err == nil {
-			docker = u.Host
+func segmentDocker(p *powerline) {
+	styles := map[string]map[string]string{
+		"up": map[string]string{
+			"icon": "^",
+			"count": "0",
+		},
+		"created": map[string]string{
+			"icon": "=",
+			"count": "0",
+		},
+		"exited": map[string]string{
+			"icon": "x",
+			"count": "0",
+		},
+	}
+
+	var docker string = ""
+	var empty bool = true
+
+	out, err := exec.Command(`docker`, `ps`, `-a`, `--format`, `"{{.Status}}"`).Output()
+	if err != nil {
+		log.Println("Cannot read status from docker daemon")
+	}
+
+	lines := strings.Split(string(out), "\n")
+
+	for _, eachline := range lines {
+		status := strings.Replace(string(eachline), `"`, "", -1)
+		status  = strings.ToLower(status)
+		stats  := strings.Split(status, " ")
+		if stats[0] != "" {
+			empty = false
+			now, _ := strconv.Atoi(styles[stats[0]]["count"])
+			styles[stats[0]]["count"] = strconv.Itoa(now + 1)
 		}
+	}
+
+	if !empty {
+		docker = fmt.Sprintf("%s:%s %s:%s %s:%s", styles["up"]["icon"],
+												  styles["up"]["count"],
+												  styles["created"]["icon"],
+												  styles["created"]["count"],
+												  styles["exited"]["icon"],
+												  styles["exited"]["count"])
 	}
 
 	if docker != "" {
